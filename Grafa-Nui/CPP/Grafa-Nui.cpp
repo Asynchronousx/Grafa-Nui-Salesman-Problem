195fdfa682eb76235b1c2a4f89d9d54606647bf0
Asynchronousx <38207760+Asynchronousx@users.noreply.github.com>
2018-10-16 12:42:43 +0200

    Rename Grafa-Nui/Grafa-Nui.cpp to Grafa-Nui/CPP/Grafa-Nui.cpp

---
+++ Grafa-Nui/CPP/Grafa-Nui.cpp
@@ -0,0 +1,13 @@
+﻿// Grafa-Nui.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "class_graph.h"
+
+int main()
+{
+	UndirectedGraph Grafa_nui;
+	Grafa_nui.createRealm();
+	Grafa_nui.CalculateDistance();
+	Grafa_nui.printFile();
+}
